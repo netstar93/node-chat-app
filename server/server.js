4ce91c592b44c79c4f7e96dbfdcdeeb96a803a0b
netstar93 <nitishnick93@gmail.com>
2018-01-06 23:55:09 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Nitish on 06 Jan.
+ */
+console.log(__dirname);
+const path = require('path');
+const express = require('express');
+const publicPath = path.join(__dirname, '../public');
+var app = express();
+app.use(express.static(publicPath));
+const port = process.env.port || 8888;
+app.listen(port , () => {
+    console.log(`server is running on port ${port}`);
+})
